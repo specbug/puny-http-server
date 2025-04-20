84365f7534e8bfa9c5f08fe71c3b5fddee53a599
Rishit Vora <rishit7.rv@gmail.com>
2025-04-20 14:44:17 +0530

    send ok

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn = server_socket.accept() # wait for client
+    sender_socket, addr = conn
+    sender_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
 
 
 if __name__ == "__main__":
